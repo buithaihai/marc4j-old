63303dc8d216d7b2e033304ecab7dfc52913d934
bpeters <>
2002-08-15 20:06:49 +0000

    Small changes to fix ANSEL to UTF-8 support.

--- src/org/marc4j/util/XmlMarcWriter.java
+++ src/org/marc4j/util/XmlMarcWriter.java
@@ -20,12 +20,7 @@
  */
 package org.marc4j.util;
 
-import java.io.Writer;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
+import java.io.*;
 import org.xml.sax.XMLReader;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -86,6 +81,7 @@ public class XmlMarcWriter {
 	String stylesheet = null;
         String schemaSource = null;
 	boolean convert = false;
+	boolean ansel = false;
         boolean dtdValidate = false;
         boolean xsdValidate = false;
 
@@ -138,9 +134,11 @@ public class XmlMarcWriter {
 		    writer = new BufferedWriter(new OutputStreamWriter(System.out, "UTF8"));
 	    } else {
 		if (convert)
-		    writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output)));
+		    writer = new BufferedWriter(new OutputStreamWriter(
+                        new FileOutputStream(output)));
 		else
-		    writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output), "UTF8"));
+		    writer = new BufferedWriter(new OutputStreamWriter(
+                        new FileOutputStream(output), "UTF8"));
 	    }
 	    MarcWriter handler = new MarcWriter(writer);
 	    if (convert)
@@ -182,6 +180,8 @@ public class XmlMarcWriter {
     }
 
     private static void usage() {
+	System.err.println("MARC4J version beta 6b, Copyright (C) 2002 Bas Peters");
+        System.err.println("Usage: org.marc4j.util.XmlMarcWriter [-options] <file.xml>");
         System.err.println("Usage: MarcXmlWriter [-options] <file.xml>");
         System.err.println("       -dtd = DTD validation");
         System.err.println("       -xsd | -xsdss <file.xsd> = W3C XML Schema validation using xsi: hints");
@@ -191,6 +191,7 @@ public class XmlMarcWriter {
         System.err.println("       -out <file> = Output using <file>");
         System.err.println("       -convert = Convert UTF-8 to ANSEL");
         System.err.println("       -usage or -help = this message");
+	System.err.println("See http://marc4j.tigris.org for more information.");
         System.exit(1);
     }
    
