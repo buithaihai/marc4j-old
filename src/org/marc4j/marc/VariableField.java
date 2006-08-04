8a3ae4e874803cc4a33685433a0c0276d9c93405
bpeters <>
2006-08-04 12:28:17 +0000

    Added getter and setter for identifier and added Comparable interface

--- src/org/marc4j/marc/VariableField.java
+++ src/org/marc4j/marc/VariableField.java
@@ -22,13 +22,34 @@ package org.marc4j.marc;
 
 import java.io.Serializable;
 
+import org.marc4j.marc.impl.VariableFieldImpl;
+
 /**
  * Represents a variable field in a MARC record.
  * 
  * @author Bas Peters
  * @version $Revision$
  */
-public interface VariableField extends Serializable {
+public interface VariableField extends Serializable, Comparable {
+
+    /**
+     * Sets the identifier.
+     * 
+     * <p>
+     * The purpose of this identifier is to provide an identifier for
+     * persistency.
+     * 
+     * @param id
+     *            the identifier
+     */
+    public void setId(Long id);
+
+    /**
+     * Returns the identifier.
+     * 
+     * @return Long - the identifier
+     */
+    public Long getId();
 
     /**
      * Returns the tag name.
