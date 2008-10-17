1bbfe46f1bbeca40cdb07467b7c420d442a5890c
haschart <>
2008-10-17 19:11:49 +0000

    Solution for xmlns duplicated entry in generated marcxml while in servlet submitted to mailing list by Alejandro Andres
    Obtained from: Alejandro Andres
    Submitted by:  Robert Haschart

--- src/org/marc4j/MarcXmlWriter.java
+++ src/org/marc4j/MarcXmlWriter.java
@@ -404,12 +404,12 @@ public class MarcXmlWriter implements MarcWriter {
         try {
             AttributesImpl atts = new AttributesImpl();
             handler.startDocument();
-            handler.startPrefixMapping("", Constants.MARCXML_NS_URI);
+            // The next line duplicates the namespace declaration for Marc XML
+            // handler.startPrefixMapping("", Constants.MARCXML_NS_URI);
             // add namespace declaration using attribute - need better solution
-            //atts.addAttribute(Constants.MARCXML_NS_URI, "xmlns", "xmlns",
-            //        "CDATA", Constants.MARCXML_NS_URI);
-            handler.startElement(Constants.MARCXML_NS_URI, COLLECTION,
-                    COLLECTION, atts);
+            atts.addAttribute(Constants.MARCXML_NS_URI, "xmlns", "xmlns",
+                              "CDATA", Constants.MARCXML_NS_URI);            
+            handler.startElement(Constants.MARCXML_NS_URI, COLLECTION, COLLECTION, atts);
         } catch (SAXException e) {
             throw new MarcException(
                     "SAX error occured while writing start document", e);
