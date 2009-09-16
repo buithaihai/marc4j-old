3adde0cf865c5118269538406df30f72c6ad8576
haschart <>
2009-09-16 18:20:45 +0000

    Submitted by:  Robert Haschart
    Minor change to the StreamReader so that the library can read from a pipe.  Previously the code used inputstream.available() to determine when it was done reading from a file, but when reading from a pipe,  available() == false  can indicate that the pipe doesn't yet have data available, but it will have data in the future.   Now it calls mark() on the stream,  tries to read a byte, which will return -1 at EOF, and block until a byte is available, and then calls reset on the stream to restore that byte to the stream.

--- src/org/marc4j/MarcStreamReader.java
+++ src/org/marc4j/MarcStreamReader.java
@@ -98,7 +98,7 @@ public class MarcStreamReader implements MarcReader {
      * Constructs an instance with the specified input stream.
      */
     public MarcStreamReader(InputStream input, String encoding) {
-        this.input = new DataInputStream(new BufferedInputStream(input));
+        this.input = new DataInputStream((input.markSupported()) ? input : new BufferedInputStream(input));
         factory = MarcFactory.newInstance();
         if (encoding != null) {
             this.encoding = encoding;
@@ -111,8 +111,10 @@ public class MarcStreamReader implements MarcReader {
      */
     public boolean hasNext() {
         try {
-            if (input.available() == 0)
+            input.mark(10);
+            if (input.read() == -1)
                 return false;
+            input.reset();
         } catch (IOException e) {
             throw new MarcException(e.getMessage(), e);
         }
@@ -339,8 +341,7 @@ public class MarcStreamReader implements MarcReader {
     }
 
     private int parseRecordLength(byte[] leaderData) throws IOException {
-        InputStreamReader isr = new InputStreamReader(new ByteArrayInputStream(
-                leaderData));
+        InputStreamReader isr = new InputStreamReader(new ByteArrayInputStream(leaderData), "ISO-8859-1");
         int length = -1;
         char[] tmp = new char[5];
         isr.read(tmp);
@@ -353,8 +354,7 @@ public class MarcStreamReader implements MarcReader {
     }
     
     private void parseLeader(Leader ldr, byte[] leaderData) throws IOException {
-        InputStreamReader isr = new InputStreamReader(new ByteArrayInputStream(
-                leaderData));
+        InputStreamReader isr = new InputStreamReader(new ByteArrayInputStream(leaderData),"ISO-8859-1");
         char[] tmp = new char[5];
         isr.read(tmp);
         //  Skip over bytes for record length, If we get here, its already been computed.
