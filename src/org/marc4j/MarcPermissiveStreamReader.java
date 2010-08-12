7e632f9de052a02a3a6dd20c9383cefba42ef48a
haschart <>
2010-08-12 16:13:33 +0000

    Submitted by:  Robert Haschart
    In the MarcPermissiveStreamReader class, if a subfield of length zero was encountered, the attempt to grab the subfield tag from it would throw a IndexOutOfBoundsException.   Now if permissive reading is enabled, the error is flagged, and the processing continues.

--- src/org/marc4j/MarcPermissiveStreamReader.java
+++ src/org/marc4j/MarcPermissiveStreamReader.java
@@ -1040,6 +1040,15 @@ public class MarcPermissiveStreamReader implements MarcReader {
                 if (code == Constants.FT)
                     break;
                 size = getSubfieldLength(bais);
+                if (size == 0)
+                {
+                    if (permissive)
+                    {
+                        errors.addError(ErrorHandler.MINOR_ERROR, "Subfield of zero length encountered, ignoring it.");
+                        continue;
+                    }
+                    throw new IOException("Subfield of zero length encountered");
+                }
                 data = new byte[size];
                 bais.read(data);
                 subfield = factory.newSubfield();
